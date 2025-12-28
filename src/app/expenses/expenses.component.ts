5224c85f84c898bf6f15649754c2da960d2d970d
Evelyn Sun <evelynsun632@gmail.com>
2025-12-28 16:22:20 +0800

    expenses

--- src/app/expenses/expenses.component.ts
+++ src/app/expenses/expenses.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit} from '@angular/core';
-import { Expense, ExpensesService } from './expenses.service';
+import { CreateExpenseRequest, Expense, ExpensesService } from './expenses.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Person,PeopleService } from '../people/people.service';
 
 @Component({
   selector: 'app-expenses',
@@ -11,11 +12,53 @@ import { FormsModule } from '@angular/forms';
 })
 export class ExpensesComponent implements OnInit {
   expenses: Expense[] = [];
+  people: Person[] = [];
 
-  constructor(private expensesService: ExpensesService){}
+  description='';
+  amount: number | null = null;
+  paidByPersonId: number | null = null;
+  participantPersonIds: number[] = [];
+
+  constructor(
+    private expensesService: ExpensesService,
+    private peopleService: PeopleService,
+  ){}
 
   ngOnInit() {
+    this.loadExpenses();
+    this.loadPeople();
+  }
+
+  loadExpenses(){
     this.expensesService.getAll().subscribe(data => this.expenses = data);
+  }
+
+  loadPeople(){
+    this.peopleService.getAll().subscribe(data => this.people = data);
+  }
+
+  addExpense(){
+    const desc = this.description.trim();
+    if (!desc) return;
+    if (this.amount == null || this.amount <= 0) return;
+    if (this.paidByPersonId == null) return;
+    if (!this.participantPersonIds || this.participantPersonIds.length === 0) return;
+
+    const payload: CreateExpenseRequest = {
+      description: desc,
+      amount: this.amount,
+      paidByPersonId: this.paidByPersonId,
+      participantPersonIds: this.participantPersonIds
+    };
+
+    this.expensesService.create(payload).subscribe(() => {
+      this.description = '';
+      this.amount = null;
+      this.paidByPersonId = null;
+      this.participantPersonIds = [];
+
+      this.loadExpenses();
+    })
   }
   
 }
--- src/app/expenses/expenses.service.ts
+++ src/app/expenses/expenses.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+export interface PersonRef{
+  id: number;
+  name: string;
+}
+
 export interface Expense {
   id: number;
   description: string;
-  amount: string;
+  amount: number;
+  paidBy: PersonRef;
+  participants: PersonRef[];
+}
+
+export interface CreateExpenseRequest {
+  description: string;
+  amount: number;
+  paidByPersonId: number;
+  participantPersonIds: number[];
 }
 
 @Injectable({
@@ -17,6 +31,10 @@ export class ExpensesService {
 
   getAll() {
     return this.http.get<Expense[]>(this.baseUrl);
+  }
+
+  create(req: CreateExpenseRequest) {
+    return this.http.post<Expense>(this.baseUrl, req);
   }
 
 }
