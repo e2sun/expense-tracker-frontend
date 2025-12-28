376f9ad8177b006e07d4b4cb2036b43900e9c2ad
Evelyn Sun <evelynsun632@gmail.com>
2025-12-28 09:57:30 +0800

    Person service and component

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PeopleService, Person } from './people.service';
 
 @Component({
   selector: 'app-people',
@@ -6,6 +7,30 @@ import { Component } from '@angular/core';
   templateUrl: './people.component.html',
   styleUrl: './people.component.css'
 })
-export class PeopleComponent {
+
+export class PeopleComponent implements OnInit {
+
+  people: Person[] = [];
+  name = '';
+
+  constructor(private peopleService: PeopleService) {}
+
+  ngOnInit() {
+    this.loadPeople();
+  }
+
+  loadPeople(){
+    this.peopleService.getAll().subscribe(data => this.people = data);
+  }
+
+  addPerson(){
+    const n = this.name.trim();
+    if (!n) return;
+
+    this.peopleService.create(n).subscribe(() => {
+      this.name = '';
+      this.loadPeople();
+    });
+  }
 
 }
--- src/app/people/people.service.ts
+++ src/app/people/people.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+export interface Person {
+  id: number;
+  name: string;
+}
+
+@Injectable({providedIn: 'root'})
 export class PeopleService {
+  private baseUrl = "http://localhost:8080/api/people";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(){
+    return this.http.get<Person[]>(this.baseUrl);
+  }
+
+  create(name: string){
+    return this.http.post<Person>(this.baseUrl, {name});
+  }
 }
